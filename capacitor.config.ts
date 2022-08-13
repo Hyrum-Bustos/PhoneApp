a8871b5c8063a9d5d83cbdf865fe6a48a067012a
HyrumBustosFernandez <hyrum.bustos@gmail.com>
2022-08-13 19:21:28 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PhoneApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
